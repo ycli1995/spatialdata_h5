4cb5aef589a5b6ebc0d5a9d6f7d4a08a3c31f748
ycli1995 <ycli1995@outlook.com>
2025-07-31 15:53:45 +0800

    No installation for spatialdata_plot

--- get_spatial_data.py
+++ get_spatial_data.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 #import matplotlib.pyplot as plt
 import pandas as pd
 import spatialdata as sd
-import spatialdata_plot as sdp  # noqa: F401
+#import spatialdata_plot as sdp  # noqa: F401
 import squidpy as sq
 #from shapely.geometry import Point
 #from spatialdata.models import Image2DModel, ShapesModel, TableModel
