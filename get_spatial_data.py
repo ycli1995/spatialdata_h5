dd892d6407d6ec00b6470676ad408a70082dc1e4
ycli1995 <ycli1995@outlook.com>
2025-07-31 15:51:31 +0800

    Don't import unused modules

--- get_spatial_data.py
+++ get_spatial_data.py
@@ -1,12 +1,12 @@
 
 import geopandas as gpd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import pandas as pd
 import spatialdata as sd
 import spatialdata_plot as sdp  # noqa: F401
 import squidpy as sq
-from shapely.geometry import Point
-from spatialdata.models import Image2DModel, ShapesModel, TableModel
+#from shapely.geometry import Point
+#from spatialdata.models import Image2DModel, ShapesModel, TableModel
 
 adata = sq.datasets.visium_hne_adata()
 
